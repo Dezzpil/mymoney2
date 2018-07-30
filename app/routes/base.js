a9d210d3df77b2a6367cbb64b5076d7dae564332
Nikita Orlov <n.dezz.orlov@gmail.com>
2018-07-31 01:22:45 +0300

    Initial commit

---
+++ app/routes/base.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let data = { title: 'Home' };
+  res.render('index', data);
+});
+
+/* GET purchases page */
+router.get('/p', function(req, res, next) {
+    let data = { title: 'Purchases' };
+    res.render('purchases', data);
+});
+
+router.post('/p', async function(req, res, next) {
+
+  const client = require('./../mongo');
+  client.
+
+
+  let value = req.body.value || 0;
+  let tags = req.body.tags || '';
+  if (req.body.comment) {
+      let comment = req.body.comment;
+  }
+
+
+  // todo save purchase to collection
+
+  res.redirect(301, '/p');
+});
+
+module.exports = router;
